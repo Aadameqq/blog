bacac4439e5df56a0e1f11aa04e2b514ab3203f7
Aadameqq <adamek5599.sluzbowy@gmail.com>
2023-07-12 16:40:41 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 		adapter: adapter({
 			pages: 'dist',
 			assets: 'dist',
-			fallback: undefined,
+			fallback: '404.html',
 			precompress: false,
 			strict: true
 		}),
@@ -17,7 +17,7 @@ const config = {
 			base: process.env.BASE_PATH
 		},
 		prerender: {
-			entries: ['/', '/404a']
+			entries: ['/', '/404']
 		}
 	}
 };
