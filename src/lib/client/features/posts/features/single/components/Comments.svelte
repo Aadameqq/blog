94f2b798d398be5d6baff0c5f515c306ad3630e8
Aadameqq <adamek5599.sluzbowy@gmail.com>
2023-07-29 20:41:58 +0200

    fix: comments

--- src/lib/client/features/posts/features/single/components/Comments.svelte
+++ src/lib/client/features/posts/features/single/components/Comments.svelte
@@ -1,21 +1,32 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let isMounted = false;
+
+	onMount(() => {
+		isMounted = true;
+	});
+</script>
+
 <div>
 	<span class="visually-hidden">Comments</span>
-	<script
-		src="https://giscus.app/client.js"
-		data-repo="Aadameqq/blog"
-		data-repo-id="R_kgDOJinSjQ"
-		data-category="Posts"
-		data-category-id="DIC_kwDOJinSjc4CYNu_"
-		data-mapping="title"
-		data-strict="1"
-		data-reactions-enabled="0"
-		data-emit-metadata="0"
-		data-input-position="top"
-		data-theme="dark_dimmed"
-		data-lang="en"
-		data-loading="lazy"
-		crossorigin="anonymous"
-		async
-	>
-	</script>
+	{#if isMounted}
+		<script
+			src="https://giscus.app/client.js"
+			data-repo="Aadameqq/blog"
+			data-repo-id="R_kgDOJinSjQ"
+			data-category="Posts"
+			data-category-id="DIC_kwDOJinSjc4CYNu_"
+			data-mapping="title"
+			data-strict="1"
+			data-reactions-enabled="0"
+			data-emit-metadata="0"
+			data-input-position="top"
+			data-theme="dark_dimmed"
+			data-lang="en"
+			data-loading="lazy"
+			crossorigin="anonymous"
+			async
+		></script>
+	{/if}
 </div>
