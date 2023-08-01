cd0ef860aedc8b1b8d6b01b4a27fa40836007b5b
Aadameqq <adamek5599.sluzbowy@gmail.com>
2023-08-01 12:48:24 +0200

    feat: navbar for mobiles

--- src/lib/client/layouts/default-layout/components/Navbar.svelte
+++ src/lib/client/layouts/default-layout/components/Navbar.svelte
@@ -2,38 +2,19 @@
 	import SocialIcons from './SocialIcons.svelte';
 	import Link from '$lib/client/components/Link.svelte';
 	import { routes } from '$lib/client/config/routes';
-
-	let isMenuOpen = false;
 </script>
 
-<header
-	class="flex laptop:justify-between w-full pt-4 laptop:pt-9 pb-4 laptop:pb-9 laptop:items-center flex-col laptop:flex-row justify-center"
->
-	<span class="flex justify-between w-full laptop:w-auto items-center">
-		<h2 class="text-xl font-bold text-gray-200 desktop:text-2xl">Adam Bryndza</h2>
-		<button
-			class="laptop:hidden"
-			on:click={() => (isMenuOpen = !isMenuOpen)}
-			aria-expanded={isMenuOpen}
-		>
-			<span class="visually-hidden">Open menu</span>
-			<span class="bi bi-list text-3xl text-gray-300" aria-hidden="true" aria-controls=""
-				><!-- --></span
+<header class="flex justify-between w-full pt-4 laptop:pt-9 pb-4 laptop:pb-9 items-center">
+	<nav>
+		<div class="flex w-full laptop:w-auto items-center">
+			<h2
+				class="text-xl font-bold text-gray-200 desktop:text-2xl hover:text-zinc-400 transition duration-300 ease-in-out"
 			>
-		</button>
+				<Link url={routes.blog({})}>Adam Bryndza</Link>
+			</h2>
+		</div>
+	</nav>
+	<span class="ml-7">
+		<SocialIcons iconClass="text-2xl" />
 	</span>
-	<div class="items-center laptop:flex {!isMenuOpen ? 'hidden' : 'h-screen'}">
-		<nav>
-			<ol
-				class="flex items-center text-lg text-gray-300 transition duration-300 ease-in-out hover:text-zinc-400"
-			>
-				<li>
-					<Link class={routes.blog({})}>Home</Link>
-				</li>
-			</ol>
-		</nav>
-		<span class="ml-7">
-			<SocialIcons iconClass="text-2xl" />
-		</span>
-	</div>
 </header>
