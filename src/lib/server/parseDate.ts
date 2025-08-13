a64b7079be1c6ebc9f82c9866e7521f43e2a1954
Aadameqq <bryndza.ad@gmail.com>
2025-08-13 19:06:02 +0200

    fix: prettier

--- src/lib/server/parseDate.ts
+++ src/lib/server/parseDate.ts
@@ -1,6 +1,4 @@
-import type { TPost } from '$lib/types/TPost';
-
 export const parseDate = (str: string): number => {
 	const [day, month, year] = str.split('.');
-	return Date.UTC(Number(year), Number(month)-1, Number(day));
-}
+	return Date.UTC(Number(year), Number(month) - 1, Number(day));
+};
